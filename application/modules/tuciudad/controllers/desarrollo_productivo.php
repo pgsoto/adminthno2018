3df4496bea80030ed63f0daf098f8a0b0a3fb494
Pedro Soto Soto <dex.pgsoto@gmail.com>
2018-05-18 18:01:22 -0400

    desarrollo productivo - subseccion - galeria

---
+++ application/modules/tuciudad/controllers/desarrollo_productivo.php
@@ -0,0 +1,256 @@
+﻿<?php if (!defined('BASEPATH')) exit('No puede acceder a este archivo');
+
+class Desarrollo_productivo extends CI_Controller
+{
+
+    private $nombre = 'Desarrollo productivo';
+    private $modulo = 56, $modulo_imagenes = 58;
+    public $img;
+
+    function __construct()
+    {
+        parent::__construct();
+
+        #define el tamaño del contenedor en la vista
+        $this->img->min_ancho_2 = 120;
+        $this->img->min_alto_2 = 120;
+
+        #define el tamaño de la imagen grande
+        $this->img->max_ancho_2 = 120 * 4;
+        $this->img->max_alto_2 = 120 * 4;
+
+        #define el tamaño del recorte
+        $this->img->recorte_ancho_2 = 120;
+        $this->img->recorte_alto_2 = 120;
+
+        #GALERIA SLIDER
+        #define el tamaño del contenedor en la vista
+        $this->img->min_ancho_1 = 1920 / 4;
+        $this->img->min_alto_1 = 720 / 4;
+
+        #define el tamaño de la imagen grande
+        $this->img->max_ancho_1 = 1920 * 4;
+        $this->img->max_alto_1 = 720 * 4;
+
+        #define el tamaño del recorte
+        $this->img->recorte_ancho_1 = 1920;
+        $this->img->recorte_alto_1 = 720;
+
+        $this->img->upload_dir = '/imagenes/modulos/tuciudad/desarrollo_productivo/';
+
+        #lib imagenes
+        $this->load->model('inicio/imagen', 'objImagen');
+
+        #Si la tabla está vacía, se inserta el primer registro
+        if (count($this->ws->listar($this->modulo)) == 0) {
+            $data = array();
+            $data['depro_estado'] = 1;
+            $data['depro_url'] = slug($this->nombre);
+            $data['depro_nombre'] = $this->nombre;
+            $this->ws->insertar($this->modulo, $data);
+            unset($data);
+        }
+
+    }
+
+    public function agregar()
+    {
+        $codigo = 1; //obligatoria primer registro
+
+        #js
+        $this->layout->js('/js/sistema/tuciudad/desarrollo_productivo/agregar.js');
+
+        #JS - Editor
+        $this->layout->js('/js/jquery/ckeditor-standard/ckeditor.js');
+        $this->layout->js('/js/jquery/ckfinder/ckfinder.js');
+
+        #js Imagen Cropic
+        $this->layout->js('/js/jquery/croppic/croppic.js');
+        $this->layout->css('/js/jquery/croppic/croppic.css');
+        $this->layout->js('/js/sistema/imagenes/simple.js');
+
+        # Contenido
+        $data = array();
+
+        if ($codigo && is_numeric($codigo)) {
+            $result = $this->ws->obtener($this->modulo, "depro_codigo = " . $codigo);
+            if ($result) {
+                $result->imagenes = $this->ws->listar($this->modulo_imagenes, "galdepro_seccion = " . $codigo);
+            }
+            #print_array($result);
+            if (!$result) {
+                redirect('/tuciudad/desarrollo-productivo/');
+            } else {
+                $data['result'] = $result;
+            }
+        }
+
+        #nav
+        if (isset($result)) {
+            $data['titulo'] = $this->nombre;
+            $this->layout->title($this->nombre);
+            $this->layout->nav(array($this->nombre => "/tuciudad/desarrollo-productivo/", $result->nombre => "/"));
+        }
+
+        #view
+        $this->layout->view('desarrollo_productivo/add', $data);
+
+    }
+
+    public function process()
+    {
+        #print_array($this->input->post());die;
+        if ($this->input->post()) {
+
+            #validaciones
+            $this->form_validation->set_rules('nombre', 'Nombre', 'required');
+
+            $this->form_validation->set_message('required', '* %s es obligatorio');
+            $this->form_validation->set_message('valid_email', '* %s no es válido');
+            $this->form_validation->set_error_delimiters('<div>', '</div>');
+
+            if (!$this->form_validation->run()) {
+                echo json_encode(array("result" => false, "msg" => validation_errors()));
+                exit;
+            } else {
+                try {
+                    $codigo = $this->input->post('codigo', true);
+
+                    $data['depro_estado'] = $this->input->post('estado');
+                    $data['depro_url'] = slug($this->input->post('nombre'));
+                    $data['depro_nombre'] = $this->input->post('nombre');
+                    $data['depro_descripcion'] = $this->input->post('descripcion');
+
+                    if ($this->input->post('ruta_interna_2')) {
+                        $data['depro_imagen_ruta_interna'] = $this->input->post('ruta_interna_2');
+                        $data['depro_imagen_ruta_grande'] = $this->input->post('ruta_grande_2');
+                    }
+
+                    # Si es una actualización el código es mayor a 0 ya que 0 es el valor predeterminado
+                    if ($codigo > 0) {
+                        if ($this->ws->actualizar($this->modulo, $data, 'depro_codigo = ' . $codigo)) {
+
+                            #GALERIA
+                            $internas = $this->input->post('ruta_interna_1');
+                            $grandes = $this->input->post('ruta_grande_1');
+                            if ($grandes) {
+                                foreach ($grandes as $k => $aux) {
+                                    if ($aux) {
+                                        $data2['galdepro_imagen_ruta_interna'] = $internas[$k];
+                                        $data2['galdepro_imagen_ruta_grande'] = $aux;
+                                        $data2['galdepro_seccion'] = $codigo;
+
+                                        $this->ws->insertar($this->modulo_imagenes, $data2);
+                                    }
+                                }
+                            }
+
+                            echo json_encode(array("result" => true, "codigo" => $codigo));
+                            exit;
+                        } else {
+                            echo json_encode(array("result" => false, "msg" => "Ha ocurrido un error inesperado. Por favor, inténtelo nuevamente."));
+                            exit;
+                        }
+                    } else {
+                        if ($codigo = $this->ws->insertar($this->modulo, $data)) {
+
+                            #GALERIA
+                            $internas = $this->input->post('ruta_interna_1');
+                            $grandes = $this->input->post('ruta_grande_1');
+                            if ($grandes) {
+                                foreach ($grandes as $k => $aux) {
+                                    if ($aux) {
+                                        $data2['galdepro_imagen_ruta_interna'] = $internas[$k];
+                                        $data2['galdepro_imagen_ruta_grande'] = $aux;
+                                        $data2['galdepro_seccion'] = $codigo->depro_codigo;
+
+                                        $this->ws->insertar($this->modulo_imagenes, $data2);
+                                    }
+                                }
+                            }
+
+                            echo json_encode(array("result" => true, "codigo" => $codigo->depro_codigo));
+                            exit;
+                        } else {
+                            echo json_encode(array("result" => false, "msg" => "Ha ocurrido un error inesperado. Por favor, inténtelo nuevamente."));
+                            exit;
+                        }
+                    }
+
+                } catch (Exception $e) {
+                    echo json_encode(array("result" => false, "msg" => "Ha ocurrido un error inesperado. Por favor, inténtelo nuevamente."));
+                    exit;
+                }
+            }
+        }
+    }
+
+    public function eliminar()
+    {
+        try {
+            $this->ws->eliminar($this->modulo, "depro_codigo = {$this->input->post('codigo')}");
+            echo json_encode(array("result" => true));
+        } catch (Exception $e) {
+            echo json_encode(array("result" => false, "msg" => "Ha ocurrido un error inesperado. Por favor, int�ntelo nuevamente."));
+        }
+    }
+
+
+    ###IMAGENES
+    public function cargar_imagen()
+    {
+
+        #se realiza la configuracion para cada imagen
+        $this->img->id = $this->input->post('id');
+        $this->objImagen->config($this->img);
+
+        $response = $this->objImagen->cargar_imagen($_FILES);
+        echo json_encode($response);
+    }
+
+    public function cortar_imagen()
+    {
+
+        #se realiza la configuracion para cada imagen
+        $this->img->id = $this->input->post('id');
+        $this->objImagen->config($this->img);
+
+        $response = $this->objImagen->cortar_imagen($_POST);
+        echo json_encode($response);
+    }
+
+    public function eliminar_imagen()
+    {
+        if ($ruta = $this->input->post('ruta_imagen')) {
+            if (file_exists($_SERVER['DOCUMENT_ROOT'] . $ruta))
+                unlink($_SERVER['DOCUMENT_ROOT'] . $ruta);
+        }
+
+        if ($codigo = $this->input->post('codigo')) {
+
+            if ($this->input->post('tipo') == 1) {
+                if ($modelo = $this->ws->obtener($this->modulo_imagenes, "galdepro_codigo = $codigo")) {
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_interna))
+                        unlink($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_interna);
+
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_grande))
+                        unlink($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_grande);
+
+                    $this->ws->eliminar($this->modulo_imagenes, "galdepro_codigo = $codigo");
+                }
+            } elseif ($this->input->post('tipo') == 2) {
+                if ($modelo = $this->ws->obtener($this->modulo, "depro_codigo = $codigo")) {
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_interna))
+                        unlink($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_interna);
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_grande))
+                        unlink($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_grande);
+                    $data['depro_imagen_ruta_interna'] = '';
+                    $data['depro_imagen_ruta_grande'] = '';
+                    $this->ws->actualizar($this->modulo, $data, "depro_codigo = $codigo");
+                }
+            }
+        }
+        echo json_encode(array("result" => true));
+    }
+
+}
---
+++ application/modules/tuciudad/controllers/subsecciones_desarrollo_productivo.php
@@ -0,0 +1,315 @@
+﻿<?php if (!defined('BASEPATH')) exit('No puede acceder a este archivo');
+
+class Subsecciones_desarrollo_productivo extends CI_Controller
+{
+
+    private $nombre = 'Subsecciones Desarrollo Productivo';
+    private $modulo = 57, $modulo_imagenes = 59, $modulo_seccion = 56;
+    public $img;
+
+    function __construct()
+    {
+        parent::__construct();
+
+        #define el tamaño del contenedor en la vista
+        $this->img->min_ancho_2 = 120;
+        $this->img->min_alto_2 = 120;
+
+        #define el tamaño de la imagen grande
+        $this->img->max_ancho_2 = 120 * 4;
+        $this->img->max_alto_2 = 120 * 4;
+
+        #define el tamaño del recorte
+        $this->img->recorte_ancho_2 = 120;
+        $this->img->recorte_alto_2 = 120;
+
+        #GALERIA SLIDER
+        #define el tamaño del contenedor en la vista
+        $this->img->min_ancho_1 = 1920 / 4;
+        $this->img->min_alto_1 = 720 / 4;
+
+        #define el tamaño de la imagen grande
+        $this->img->max_ancho_1 = 1920 * 4;
+        $this->img->max_alto_1 = 720 * 4;
+
+        #define el tamaño del recorte
+        $this->img->recorte_ancho_1 = 1920;
+        $this->img->recorte_alto_1 = 720;
+
+        $this->img->upload_dir = '/imagenes/modulos/tuciudad/desarrollo-productivo/subsecciones/';
+
+        #lib imagenes
+        $this->load->model('inicio/imagen', 'objImagen');
+    }
+
+    public function index()
+    {
+        $seccion = 1;
+
+        # Contenido
+        $data = array();
+
+        $data['seccion'] = $seccion;
+
+        #Title
+        $data['titulo'] = $this->nombre;
+        $this->layout->title($this->nombre);
+
+        #js
+        $this->layout->js('/js/sistema/tuciudad/desarrollo_productivo/subsecciones/index.js');
+
+        $where = $and = "";
+        $url = "";
+
+        $where .= "subdepro_visible = 1";
+        $and = " and ";
+
+        if (count($_GET) > 0)
+            $url = '?' . http_build_query($_GET, '', "&");
+
+        $config['uri_segment'] = 5;
+        $config['base_url'] = '/tuciudad/desarrollo-productivo/subsecciones/' . $seccion . '/';
+        $config['per_page'] = 20;
+        $config['total_rows'] = count($this->ws->listar($this->modulo, $where));
+        $config['suffix'] = '/' . $url;
+        $config['first_url'] = $config['base_url'] . $url;
+        $this->pagination->initialize($config);
+
+        #obtiene el numero de pagina
+        $pagina = ($this->uri->segment($config['uri_segment'])) ? $this->uri->segment($config['uri_segment']) - 1 : 0;
+
+        #contenido
+        $this->ws->order("subdepro_orden ASC");
+        $this->ws->limit($config['per_page'], ($config['per_page'] * $pagina));
+        $data["result"] = $this->ws->listar($this->modulo, $where);
+        $data['pagination'] = $this->pagination->create_links();
+
+        $seccion = $this->ws->obtener($this->modulo_seccion, "depro_codigo = " . $seccion);
+
+        #Nav
+        $this->layout->nav(array('Desarrollo Productivo' => '/tuciudad/desarrollo-productivo/', $this->nombre => '/'));
+
+        #view
+        $this->layout->view('desarrollo_productivo/subsecciones/index', $data);
+    }
+
+    public function agregar($seccion = false, $codigo = false)
+    {
+        # Contenido
+        $data = array();
+
+        $data['seccion'] = $seccion;
+
+        #js
+        $this->layout->js('/js/sistema/tuciudad/desarrollo_productivo/subsecciones/agregar.js');
+
+        #JS - Editor
+        $this->layout->js('/js/jquery/ckeditor-standard/ckeditor.js');
+        $this->layout->js('/js/jquery/ckfinder/ckfinder.js');
+
+        #js Imagen Cropic
+        $this->layout->js('/js/jquery/croppic/croppic.js');
+        $this->layout->css('/js/jquery/croppic/croppic.css');
+        $this->layout->js('/js/sistema/imagenes/simple.js');
+
+        if ($codigo && is_numeric($codigo)) {
+            $result = $this->ws->obtener($this->modulo, "subdepro_codigo = " . $codigo);
+            if ($result) {
+                $result->mapa_coor = explode(",", $result->mapa);
+                $result->imagenes = $this->ws->listar($this->modulo_imagenes, "galsubdepro_subseccion = " . $codigo);
+            }
+            #print_array($result);
+            if (!$result) {
+                redirect('/tuciudad/desarrollo-productivo/subsecciones/');
+            } else {
+                $data['result'] = $result;
+            }
+        }
+
+        $seccion = $this->ws->obtener($this->modulo_seccion, "depro_codigo = " . $seccion);
+
+        #nav
+        if (isset($result)) {
+            $data['titulo'] = 'Editar ' . $this->nombre;
+            $this->layout->title('Editar ' . $this->nombre);
+            $this->layout->nav(array("DAS" => "/tuciudad/desarrollo-productivo/", $this->nombre => "/tuciudad/desarrollo-productivo/subsecciones/" . $seccion->codigo . "/", "Editar " . $result->nombre => "/"));
+        } else {
+            $data['titulo'] = 'Agregar ' . $this->nombre;
+            $this->layout->title('Agregar ' . $this->nombre);
+            $this->layout->nav(array("DAS" => "/tuciudad/desarrollo-productivo/", $this->nombre => "/tuciudad/desarrollo-productivo/subsecciones/" . $seccion->codigo . "/", "Agregar " . $this->nombre => "/"));
+        }
+
+        #view
+        $this->layout->view('desarrollo_productivo/subsecciones/add', $data);
+
+    }
+
+    public function process()
+    {
+        #print_array($this->input->post());#die;
+        if ($this->input->post()) {
+
+            #validaciones
+            $this->form_validation->set_rules('nombre', 'Nombre', 'required');
+            $this->form_validation->set_rules('orden', 'Orden', 'required');
+            $this->form_validation->set_rules('estado', 'Estado', 'required');
+
+            $this->form_validation->set_message('required', '* %s es obligatorio');
+            $this->form_validation->set_error_delimiters('<div>', '</div>');
+
+            if (!$this->form_validation->run()) {
+                echo json_encode(array("result" => false, "msg" => validation_errors()));
+                exit;
+            } else {
+                try {
+                    $codigo = $this->input->post('codigo', true);
+
+                    $data['subdepro_estado'] = $this->input->post('estado');
+                    $data['subdepro_url'] = slug($this->input->post('nombre'));
+                    $data['subdepro_nombre'] = $this->input->post('nombre');
+                    $data['subdepro_orden'] = $this->input->post('orden');
+                    $data['subdepro_descripcion'] = $this->input->post('descripcion');
+                    $data['subdepro_encargado'] = $this->input->post('encargado');
+                    $data['subdepro_secretaria'] = $this->input->post('secretaria');
+                    $data['subdepro_telefono'] = $this->input->post('telefono');
+                    $data['subdepro_email'] = $this->input->post('email');
+                    $data['subdepro_direccion'] = $this->input->post('direccion');
+
+                    if ($this->input->post('ruta_interna_2')) {
+                        $data['subdepro_imagen_ruta_interna'] = $this->input->post('ruta_interna_2');
+                        $data['subdepro_imagen_ruta_grande'] = $this->input->post('ruta_grande_2');
+                    }
+
+                    if ($this->input->post("mapa"))
+                        $data['subdepro_mapa'] = str_replace(array("(", ")", " "), "", $this->input->post("mapa"));
+
+                    $data['subdepro_seccion'] = $this->input->post('seccion');
+
+                    # Si es una actualización el código es mayor a 0 ya que 0 es el valor predeterminado
+                    if ($codigo > 0) {
+                        if ($this->ws->actualizar($this->modulo, $data, 'subdepro_codigo = ' . $codigo)) {
+
+                            #GALERIA
+                            $internas = $this->input->post('ruta_interna_1');
+                            $grandes = $this->input->post('ruta_grande_1');
+                            if ($grandes) {
+                                foreach ($grandes as $k => $aux) {
+                                    if ($aux) {
+                                        $data2['galsubdepro_imagen_ruta_interna'] = $internas[$k];
+                                        $data2['galsubdepro_imagen_ruta_grande'] = $aux;
+                                        $data2['galsubdepro_subseccion'] = $codigo;
+
+                                        $this->ws->insertar($this->modulo_imagenes, $data2);
+                                    }
+                                }
+                            }
+
+                            echo json_encode(array("result" => true, "codigo" => $codigo, "seccion" => $this->input->post('seccion')));
+                            exit;
+                        } else {
+                            echo json_encode(array("result" => false, "msg" => "Ha ocurrido un error inesperado. Por favor, inténtelo nuevamente."));
+                            exit;
+                        }
+                    } else {
+                        if ($codigo = $this->ws->insertar($this->modulo, $data)) {
+
+                            #GALERIA
+                            $internas = $this->input->post('ruta_interna_1');
+                            $grandes = $this->input->post('ruta_grande_1');
+                            if ($grandes) {
+                                foreach ($grandes as $k => $aux) {
+                                    if ($aux) {
+                                        $data2['galsubdepro_imagen_ruta_interna'] = $internas[$k];
+                                        $data2['galsubdepro_imagen_ruta_grande'] = $aux;
+                                        $data2['galsubdepro_subseccion'] = $codigo->subdepro_codigo;
+
+                                        $this->ws->insertar($this->modulo_imagenes, $data2);
+                                    }
+                                }
+                            }
+
+                            echo json_encode(array("result" => true, "codigo" => $codigo->subdepro_codigo, "seccion" => $this->input->post('seccion')));
+                            exit;
+                        } else {
+                            echo json_encode(array("result" => false, "msg" => "Ha ocurrido un error inesperado. Por favor, inténtelo nuevamente."));
+                            exit;
+                        }
+                    }
+
+                } catch (Exception $e) {
+                    echo json_encode(array("result" => false, "msg" => "Ha ocurrido un error inesperado. Por favor, inténtelo nuevamente."));
+                    exit;
+                }
+            }
+        }
+    }
+
+    public function eliminar()
+    {
+        try {
+            $this->ws->eliminar($this->modulo, "subdepro_codigo = {$this->input->post('codigo')}");
+            echo json_encode(array("result" => true));
+        } catch (Exception $e) {
+            echo json_encode(array("result" => false, "msg" => "Ha ocurrido un error inesperado. Por favor, int�ntelo nuevamente."));
+        }
+    }
+
+
+    ###IMAGENES
+    public function cargar_imagen()
+    {
+
+        #se realiza la configuracion para cada imagen
+        $this->img->id = $this->input->post('id');
+        $this->objImagen->config($this->img);
+
+        $response = $this->objImagen->cargar_imagen($_FILES);
+        echo json_encode($response);
+    }
+
+    public function cortar_imagen()
+    {
+
+        #se realiza la configuracion para cada imagen
+        $this->img->id = $this->input->post('id');
+        $this->objImagen->config($this->img);
+
+        $response = $this->objImagen->cortar_imagen($_POST);
+        echo json_encode($response);
+    }
+
+    public function eliminar_imagen()
+    {
+        if ($ruta = $this->input->post('ruta_imagen')) {
+            if (file_exists($_SERVER['DOCUMENT_ROOT'] . $ruta))
+                unlink($_SERVER['DOCUMENT_ROOT'] . $ruta);
+        }
+
+        if ($codigo = $this->input->post('codigo')) {
+
+            if ($this->input->post('tipo') == 1) {
+                if ($modelo = $this->ws->obtener($this->modulo_imagenes, "galsubdepro_codigo = $codigo")) {
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_interna))
+                        unlink($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_interna);
+
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_grande))
+                        unlink($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_grande);
+
+                    $this->ws->eliminar($this->modulo_imagenes, "galsubdepro_codigo = $codigo");
+                }
+            } elseif ($this->input->post('tipo') == 2) {
+                if ($modelo = $this->ws->obtener($this->modulo, "subdepro_codigo = $codigo")) {
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_interna))
+                        unlink($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_interna);
+                    if (file_exists($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_grande))
+                        unlink($_SERVER['DOCUMENT_ROOT'] . $modelo->imagen_ruta_grande);
+                    $data['subdepro_imagen_ruta_interna'] = '';
+                    $data['subdepro_imagen_ruta_grande'] = '';
+                    $this->ws->actualizar($this->modulo, $data, "subdepro_codigo = $codigo");
+                }
+            }
+        }
+        echo json_encode(array("result" => true));
+    }
+
+}
